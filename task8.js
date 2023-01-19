2cf9b5fd6344c0fe39926eccdfe945ba9bc6fac4
Henning Valderhaug <hvalderhaug@gmail.com>
2023-01-19 22:14:45 +0000

    changed var name to spell out Array

--- task8.js
+++ task8.js
@@ -1,9 +1,9 @@
 // Just some random code for the purposes of the task.
 
 // a random array
-let randomArr = [1, 5, 3, 6, 10];
+let randomArray = [1, 5, 3, 6, 10];
 
 // log all elements multiplied by 5
-randomArr.forEach((element) => {
+randomArray.forEach((element) => {
   console.log(element * 5);
 });
