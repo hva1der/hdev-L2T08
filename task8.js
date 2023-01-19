dcbb46da1f309f87a499fc1872db1405f7c228d0
Henning Valderhaug <hvalderhaug@gmail.com>
2023-01-19 21:43:18 +0000

    added some random content

---
+++ task8.js
@@ -0,0 +1,9 @@
+// Just some random code for the purposes of the task.
+
+// a random array
+let randomArr = [1, 5, 3, 6, 10];
+
+// log all elements multiplied by 5
+randomArr.forEach((element) => {
+  console.log(element * 5);
+});
